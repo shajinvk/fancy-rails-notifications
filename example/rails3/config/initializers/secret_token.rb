fb624dd05c7afab0af21517c19bf4c5c5a51c9d3
Michael <michael@luckeneder.net>
2011-05-11 16:34:48 +0100

    removed example

--- example/rails3/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Example::Application.config.secret_token = '94f5b93e3cba1312cddddc0b26fee7711cc93547807a6203e9ff9b564f3926d6829309c97d18908cff9ff6b06388b729d6c77da0d5b944198d140c00fbc15dbd'
